416aed499d513264adcff098fe7b9680a4dded61
Marissa <Marissa.Bart@cuw.edu>
2017-03-09 10:40:25 -0600

    adding to dictionary and working on observer design

--- MakingChatroom/Client.cs
+++ MakingChatroom/Client.cs
@@ -42,7 +42,7 @@ namespace Chatroom
             // Retrive the Name of HOST
             string hostName = Dns.GetHostName();
             // Get the IP
-            myIP = Dns.GetHostEntry(hostName).AddressList[1].ToString();
+            myIP = Dns.GetHostEntry(hostName).AddressList[2].ToString();
         }
         public void ConnectToServer()
         {
